c4fc456ddcec2ff19c7a176a3553a44b106010e2
Marcin Sarna <sarna077@gmail.com>
2023-02-10 23:35:19 +0100

    Finish first step

--- magazyn.py
+++ magazyn.py
@@ -1,7 +1,7 @@
 print("Hello, it is nice to see you. You can choose show, exit, ")
-items = [{"name:": [1], "quantity:": [2], "unit:": [3], "unit_price:": [4]},
-         {"name:": [11], "quantity:": [22], "unit:": [33], "unit_price:": [44]},
-         {"name:": [111], "quantity:": [222], "unit:": [333], "unit_price:": [444]}]
+items = [{"name:": "Pomidory", "quantity:": 20, "unit:": "kg", "unit_price:": 4.2},
+         {"name:": "Ogórki", "quantity:": 22, "unit:": "kg", "unit_price:": 5.0},
+         {"name:": "Marchewka", "quantity:": 15, "unit:": "kg", "unit_price:": 6.5}]
 
 
 
@@ -15,4 +15,5 @@ while odp != "exit":
         print("Name\tQuantity\tUnit\tPrice (PLN)")
         for i in items:
             for z in i.values():
-                print(z, "\t")
+                print(z, "\t", end = " ")
+            print("")
